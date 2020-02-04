13cf9c289155e53506a640bd86fc599af8d84e97
Muehlberger <MUEHLMI1@novartis.net>
2020-02-04 10:49:25 +0100

    First version, tested with matrix mat<-matrix(c(1,2,3,2,1,2,3,3,1),nrow=3,ncol=3)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Computing the inverse of a invertable square matrix
+## with cached results
 
-## Write a short comment describing this function
-
+## makeCacheMatrix: creates a vector with the 
+##   set, get, setsolve, getsolve functions
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 
 }
 
 
 ## Write a short comment describing this function
-
+## cacheSolve: creates the invers of a square inversable matrix
+##   with cached elements
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
